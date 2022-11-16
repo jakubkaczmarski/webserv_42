5d68eb4ee05589424da69b97bb8f617bcf8db0b7
kis619 <kis619@abv.bg>
2022-11-16 13:19:42 +0100

    parsing the first line and being able to determine the type of request

--- includes/placeholder.hpp
+++ includes/placeholder.hpp
@@ -1,6 +1,14 @@
 #ifndef PLACEHOLDER_HPP
 #define PLACEHOLDER_HPP
 #include "webserv.hpp"
+// #include <iostream>
+
+typedef struct t_request
+{
+	std::string		method;
+	std::string		URI;
+	std::string		httpVers;
+}	s_request;
 
 class placeholder
 {
@@ -9,7 +17,8 @@ class placeholder
 		int					connectionSocket;
 		struct sockaddr_in	serverAddress;
 		int					sizeOfServerAddress = sizeof(serverAddress);
-		std::string			currRequest;
+		std::string			fullRequest;
+		s_request			currRequest;
 
 
 		void	failTest( int check, std::string message )
@@ -50,6 +59,48 @@ class placeholder
 						"listen() of Server Socket");
 		}; // probably here address
 
+		s_request	&fillRequestStruct(std::string &fullRequest)
+		{
+			std::string		firstLine = fullRequest.substr(0, fullRequest.find('\n'));
+			currRequest.method = fullRequest.substr(0, fullRequest.find(' '));
+			std::string		tempstr = fullRequest.substr(fullRequest.find(' ') + 1, fullRequest.size());
+			currRequest.URI = tempstr.substr(0, tempstr.find(' '));
+			currRequest.httpVers = tempstr.substr(tempstr.find(' ') + 1, tempstr.size());
+			return (currRequest);
+
+		}
+
+		void		handleRequest(std::string &fullRequest)
+		{
+			s_request		currRequest = fillRequestStruct(fullRequest);
+
+
+			if (currRequest.method.compare("GET") == 0)
+			{
+				cout << "its a get request :)" << endl;
+			}
+			else if (currRequest.method.compare("POST") == 0)
+			{
+				cout << "its a post request :)" << endl;
+			}
+			else if (currRequest.method.compare("DELETE") == 0)
+			{
+				cout << "its a delete request :)" << endl;
+			}
+			else
+			{
+				cout << "We should throw an error code with a message that we do not support this method" << endl;
+			}
+
+
+			cout << "method = " << currRequest.method << endl;
+			cout << "URI = " << currRequest.URI << endl;
+			cout << "httpVers = " << currRequest.httpVers << endl;
+
+		}
+
+
+
 
 	public:
 		placeholder()
@@ -75,15 +126,20 @@ class placeholder
 			requestSocket = accept(serverSocket, (SA *) NULL, NULL);
 			failTest(requestSocket, "accept() Socket");
 			memset(receivingBuffer, 0, MAX_LINE + 1);	
-			currRequest.clear();
+			fullRequest.clear();
 			while(((recvReturn = recv(requestSocket, receivingBuffer, MAX_LINE, 0)) > 0))
 			{
 				failTest(recvReturn, "Reading into receivingBuffer out of requestSocket");
-				currRequest.append(receivingBuffer);
+				fullRequest.append(receivingBuffer);
 				if (receivingBuffer[recvReturn - 1] == '\n' && receivingBuffer[recvReturn - 2] == '\r')
 					break;
 				memset(receivingBuffer, 0, MAX_LINE);
 			}
+
+			handleRequest(fullRequest);
+			// funciton to determine what kind of request
+
+			// here we call the corresponding request funciton
 
 			// proccess the request here to get an answer
 
@@ -93,7 +149,7 @@ class placeholder
 			failTest(close(requestSocket),
 						"Sending answer to Request to requestSocket");
 			cout << "This is the full Request" << RESET_LINE;
-			cout << endl << currRequest << endl << endl;
+			cout << endl << fullRequest << RED << "<<here is the end>>" << RESET_LINE;
 		}
 };
 
