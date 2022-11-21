e66fe770357912baeb8b8559e55446a250cf995d
kis619 <kis619@abv.bg>
2022-11-21 17:21:34 +0100

    commented out thing which are not ready yet

--- src/config.cpp
+++ src/config.cpp
@@ -1,46 +1,46 @@
-#include "../includes/config.hpp"
-#include <fstream> //for opening files
+// #include "../includes/config.hpp"
+// #include <fstream> //for opening files
 
-config::config()
-{
-	initDefaultConfig();
-}
+// config::config()
+// {
+// 	initDefaultConfig();
+// }
 
-config::config(char *confPath)
-{
-	// std::string		confPathStr = confPath;
+// config::config(char *confPath)
+// {
+// 	// std::string		confPathStr = confPath;
 
-	std::ifstream	confStream;
-	std::string		workingLine;
+// 	std::ifstream	confStream;
+// 	std::string		workingLine;
 
 
-	confStream = open(confPath);
-}
+// 	confStream = open(confPath);
+// }
 
-config::~config()
-{
+// config::~config()
+// {
 
-}
+// }
 
-confMapType&	config::getConfigMap( void )
-{
-	return (configMap);
-} 
+// confMapType&	config::getConfigMap( void )
+// {
+// 	return (configMap);
+// } 
 
-void	config::initDefaultConfig( void )
-{
-	configMap["serverName"] = "defaultServerName";
-	configMap["host"] = "0.0.0.0";
-	configMap["port"] = "1999";
-	configMap["clientMaxBodySize"] = "1000000";
-	configMap["errorPage"] = "/database/Error_404.png";
-	configMap["methods"] = "GET";
-	configMap["root"] = "/workspaces/webserv_42";
-	configMap["dir"] = "/";
-	// configMap[""] = "";
-	// configMap[""] = "";
-	// configMap[""] = "";
-	// configMap[""] = "";
-	// configMap[""] = "";
-}
+// void	config::initDefaultConfig( void )
+// {
+// 	configMap["serverName"] = "defaultServerName";
+// 	configMap["host"] = "0.0.0.0";
+// 	configMap["port"] = "1999";
+// 	configMap["clientMaxBodySize"] = "1000000";
+// 	configMap["errorPage"] = "/database/Error_404.png";
+// 	configMap["methods"] = "GET";
+// 	configMap["root"] = "/workspaces/webserv_42";
+// 	configMap["dir"] = "/";
+// 	// configMap[""] = "";
+// 	// configMap[""] = "";
+// 	// configMap[""] = "";
+// 	// configMap[""] = "";
+// 	// configMap[""] = "";
+// }
 
