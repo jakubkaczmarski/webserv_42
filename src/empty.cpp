762f4491b326f77e66b27a0e4331e86e8a56bc1a
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2022-11-27 14:10:54 +0000

    Working thingy

--- src/empty.cpp
+++ src/empty.cpp
@@ -56,9 +56,9 @@ void	server::responseHeader( std::vector<connecData*>::iterator it )
 	// parse and send header to client
 	// open fd into the (*it)->response.body_fd for the body
 	(*it)->response.headers = "HTTP/1.1 200 OK\n"
-"Content-Length: 10\n"
+"Content-Length: 19000\n"
 "Content-Type: text/html\n"
-"Connection: Closed\n\n";
+"Connection: Closed\n\n\n";
 	send((*it)->socket, (*it)->response.headers.c_str(), (*it)->response.headers.length(), 0);
 	FILE *f = fopen("database/default_index.html", "rb");
 	(*it)->response.body_fd = fileno(f);
@@ -72,7 +72,7 @@ void	server::endResponse( struct epoll_event ev )
 	connections.erase(it);
 	epoll_ctl(epollFD, EPOLL_CTL_DEL, ev.data.fd, &ev);
 
-	
+	close(ev.data.fd);
 }
 
 void	server::confusedEpoll( struct epoll_event ev )
