87fb5fabc77873c56456c6f96a0ea747f1f4ac8c
kis619 <kis619@abv.bg>
2022-12-04 00:40:09 +0000

    updated the script

--- cgi-bin/put_photo_in_cat.py
+++ cgi-bin/put_photo_in_cat.py
@@ -2,30 +2,20 @@
 
 import os
 
-#oppening the html for reading
-# file = open("/workspaces/webserv_42/database/default_index_.html", "r")
-file = open("/home/kis619/webserv_42/database/default_index_.html", "r")
 
-# #initialising an empty string, getting the intra login, prepraing the path
-replaced_content = ""
-intra = os.environ["QUERY_STRING"].split('=')[1]
-# print(intra)
-# new_path = "/workspaces/webserv_42/database/intraPictures" + intra + '.jpg'
-new_path = "/home/kis619/webserv_42/database/intraPictures/" + intra + ".jpg"
+file = open("/workspaces/webserv_42/database/default_index_.html", "r")			#oppening the html for reading
 
-#looping through the file
-for line in file:
 
-    #replacing the texts
-    new_line = line.replace("/database/UFF.png", new_path)
+replaced_content = ""															#initialising an empty string 
+intra = os.environ["QUERY_STRING"].split('=')[1]								#getting the intra login
+new_path = "/workspaces/webserv_42/database/intraPictures/" + intra + '.jpg'	#prepraing the path
 
-    #concatenate the new string and add an end-line break
-    replaced_content = replaced_content + new_line
 
-    
-#close the file
-file.close()
+for line in file:																#line by line reading
+    new_line = line.replace("/database/UFF.png", new_path)						#replacing the text
+    replaced_content = replaced_content	+ new_line								#adding the new line
 
-# print(os.environ)
+file.close()																	#close the file
+
 # print("Content-Type: text/html")
-print(replaced_content)
+print(replaced_content)															#print result
